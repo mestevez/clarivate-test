5ef4ae41bc3ff4893c1f4f30ff9b76fd478b2663
marc <mestevez85@gmail.com>
2020-12-10 00:13:41 +0100

    Initialize project files

---
+++ src/main/java/com/clarivate/testrestapi/TestrestapiApplication.java
@@ -0,0 +1,13 @@
+package com.clarivate.testrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestrestapiApplication.class, args);
+	}
+
+}
